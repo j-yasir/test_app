8e7fd3cbd9e396446bfc4573b5e8ecfc1b0fa21f
j-yasir <123079015+j-yasir@users.noreply.github.com>
2023-12-09 22:27:52 +0500

    Update test.py

--- test.py
+++ test.py
@@ -1,2 +1,29 @@
-for i in range(0, 10):
-    print("yasir")
+import turtle
+
+# Set up the turtle
+screen = turtle.Screen()
+screen.bgcolor("black")
+screen.title("Cool Python Drawing")
+
+pen = turtle.Turtle()
+pen.speed(2)
+pen.color("cyan")
+
+# Function to draw a star
+def draw_star(size):
+    for _ in range(5):
+        pen.forward(size)
+        pen.right(144)
+
+# Draw the star
+pen.penup()
+pen.goto(-50, 0)
+pen.pendown()
+
+draw_star(100)
+
+# Hide the turtle
+pen.hideturtle()
+
+# Keep the window open
+turtle.mainloop()
